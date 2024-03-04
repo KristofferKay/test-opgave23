1ca413fa54bb89801454bd33ed02c4911c75a98a
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-03-04 09:44:19 +0100

    3rd commit

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println("Hi");
+        System.out.println("There");
 
         System.out.println("hey");
     }
